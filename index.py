ef3877b6003916fd737e248add978426d58204aa
zombieaattak2009 <dejavudiscord146@gmail.com>
2022-09-25 16:51:59 +0700

    Fix #1
    Huge fixes. There will be more bug fixes going on.

--- index.py
+++ index.py
@@ -1,4 +1,15 @@
 
+def search(list, platform):
+    for i in range(len(list)):
+        if list[i] == platform:
+            return True
+    return False
+
+def check(list, sum):
+    for i in range(len(list)):
+        if list[i] == sum:
+            return True
+    return False
 
 n=input()
 n=int(n)
@@ -8,12 +19,22 @@ a = list(range(1,n))
 b = a[0]+a[1]+a[2]
 c = total-b
 
-for i in range(len(a)):
-    if a[i] == c:
-        print("Found")
-        answer=b+c
-        break
+if check(a, total):
+    print("Yes")
+    exit()
+else:
+    None
 
-print(answer)
+
+if search(a, c):
+    print("True")
+    answer=b+c
+    print(answer)
+else: 
+    print("false")
+    print("-1")
+
+
+
 
 
